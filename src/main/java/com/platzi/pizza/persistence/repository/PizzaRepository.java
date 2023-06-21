febd32b55ba0e4789217eb526cf4f296bda5fbac
Jorge Gomez <jorgegomezrios@gmail.com>
2023-06-20 19:40:32 -0600

    Added audit atributes to the pizza table and applied audit listeners in every modifying query [ the base project its completedgit status ]

--- src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
@@ -1,7 +1,11 @@
 package com.platzi.pizza.persistence.repository;
 
 import com.platzi.pizza.persistence.entity.PizzaEntity;
+import com.platzi.pizza.service.dto.UpdatePizzaPriceDto;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +18,9 @@ public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer
     List<PizzaEntity> findAllByAvailableTrueAndPriceGreaterThanEqual(int price);
     List<PizzaEntity> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(double price);
     int countByVeganTrue();
+    @Query(value = "UPDATE pizza "+
+            "SET price = :#{#newPizzaPrice.newPrice} "+
+            "WHERE id_pizza = :#{#newPizzaPrice.pizzaId} ", nativeQuery = true)
+    @Modifying
+    void updatePrice(@Param("newPizzaPrice") UpdatePizzaPriceDto newPizzaPrice);
 }
